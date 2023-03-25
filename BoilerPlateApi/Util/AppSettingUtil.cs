5eb2c03e9f47fae7cd3d09234d7ab591d3c32a8d
MrT <taimourmughal67@gmail.com>
2023-03-25 12:29:07 +0500

    Blazor Project Added

---
+++ BoilerPlateApi/Util/AppSettingUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace BoilerPlateApi.Util
+{
+    public class AppSettingUtil
+    {
+        public class Connections
+        {
+            public const string SQLConnectionString = "ConnStr";
+        }
+    }
+}
